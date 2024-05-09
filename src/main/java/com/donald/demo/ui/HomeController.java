eeb3b718ae32d3318117929d47b0afac6ff56a56
Donald Forbes <donald@tannoch.local>
2024-05-09 14:57:08 +0100

    Update to incorporate the jQuery libraries

--- src/main/java/com/donald/demo/ui/HomeController.java
+++ src/main/java/com/donald/demo/ui/HomeController.java
@@ -10,7 +10,8 @@ import java.util.ArrayList;
 
 import com.donald.demo.ui.model.Demo;
 import com.donald.demo.ui.model.Demonstration;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
@@ -24,12 +25,26 @@ public class HomeController {
         model.addAttribute("listDemos", theDemos.getDemo());
         return "welcome";
     }
-    
+
     @GetMapping("/hello-world")
     public String getHelloWorld(Model model) {
         model.addAttribute("sample", "Hello World");
         return new String("hello-world");
     }
+
+    @GetMapping("/hello")
+    public String getHello() {
+        System.out.println("Hello running from a get request");
+        return new String("/");
+    }
+    @PostMapping("/post-hello")
+    public String postMethodName(@RequestBody String entity) {
+        //TODO: process POST request
+        System.out.println("post-hello Entered");
+
+        System.out.println(entity.toString());
+        
+        return "hello-world";
+    }
     
 }
-
