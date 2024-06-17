393ae29da11e2b31fef6d60809b506f10bbb2130
Donald Forbes <donald@tannoch.local>
2024-06-17 13:05:02 +0100

    Minor refactor to remove extra params and tidy controllers.

--- src/main/java/com/donald/demo/ui/controllers/HomeController.java
+++ src/main/java/com/donald/demo/ui/controllers/HomeController.java
@@ -62,86 +62,8 @@ public class HomeController {
         return new String("hello-world");
     }
 
-    @GetMapping("/money-transfer-welcome")
-    public String getMoneyTransferWelcome(Model model) {
-        model.addAttribute("appName","Money Transfer");
-        model.addAttribute("accounts", toAccounts.getDestinationAccounts());
-        model.addAttribute("options", toAccounts.getWorkflowOptions());
-        return new String("money-transfer-welcome");
-    }
-
-    @GetMapping("/money-transfer-details")
-    public String getMoneyTransferDetails (@RequestParam(required = false) String workflowID, Model model) {
-        System.out.println("money-transfer-details method entry.");
-        System.out.println("Request parameter is -" + workflowID);
-        model.addAttribute("appName","Money Transfer");
-        model.addAttribute("selectedWorkflow", workflowID);
-        // Setup a new model state object that is returned if workflow identified is not found.
-        MoneyTransferState moneyTransferState = new MoneyTransferState();
-        MoneyTransferResponse moneyTransferResp = new MoneyTransferResponse();
-        moneyTransferResp.setChargeId("Not yet set");
-        moneyTransferState.setMoneyTransferResponse(moneyTransferResp);
 
 
-        if ((workflowID != null) && (workflowID.length() > 1))
-        {
-               WorkflowStub wfStub = client.newUntypedWorkflowStub(workflowID);
-               moneyTransferState = wfStub.query("transferStatus", MoneyTransferState.class);
-               logger.debug("moneyTransferState Returned: " +moneyTransferState.toString());
-        }
-
-        model.addAttribute("moneyTransferState", moneyTransferState);
-        
-        WorkflowServiceStubs wfStubs = client.getWorkflowServiceStubs();
-        ListOpenWorkflowExecutionsResponse openResponse =
-            wfStubs.blockingStub()
-                   .listOpenWorkflowExecutions(
-                    ListOpenWorkflowExecutionsRequest.newBuilder()
-                      //.setStartTimeFilter(StartTimeFilter.newBuilder().setEarliestTime(TemporalClient.getOneHourAgo()).build())
-                      .setTypeFilter(WorkflowTypeFilter.newBuilder().setName("TransferMoneyWorkflow").build())
-                      .setNamespace(client.getOptions().getNamespace())
-                      .build()
-                );
-             
-        ListClosedWorkflowExecutionsResponse closedResponse =
-          wfStubs.blockingStub()
-                 .listClosedWorkflowExecutions(
-                    ListClosedWorkflowExecutionsRequest.newBuilder()
-                                    .setStartTimeFilter(StartTimeFilter.newBuilder().setEarliestTime(TemporalClient.getOneHourAgo()).build())
-                                    .setTypeFilter(WorkflowTypeFilter.newBuilder().setName("TransferMoneyWorkflow").build())
-                                    .setNamespace(client.getOptions().getNamespace())
-                                    .build()
-                 );
-
-
-        List<WorkflowStatus> workflowStatii = new ArrayList<>();
-
-        logger.debug("We have "+ openResponse.getExecutionsList().size() + " open workflows and " + closedResponse.getExecutionsList().size() + " Closed");
-        WorkflowStatus aWfStatus;
-        for (WorkflowExecutionInfo wfExecutionInfo : openResponse.getExecutionsList())  {
-            aWfStatus = new WorkflowStatus();
-            aWfStatus.setWorkflowId(wfExecutionInfo.getExecution().getWorkflowId());
-            aWfStatus.setWorkflowStatus(wfExecutionInfo.getStatus().toString());
-            aWfStatus.setUrl(TemporalClient.getWorkflowUrl(aWfStatus.getWorkflowId(), wfStubs.getOptions().getTarget(), client.getOptions().getNamespace()));
-            
-        workflowStatii.add(aWfStatus);
-        }
-        for (WorkflowExecutionInfo wfExecutionInfo : closedResponse.getExecutionsList())  {
-            aWfStatus = new WorkflowStatus();
-            aWfStatus.setWorkflowId(wfExecutionInfo.getExecution().getWorkflowId());
-            aWfStatus.setWorkflowStatus(wfExecutionInfo.getStatus().toString());
-            aWfStatus.setUrl(TemporalClient.getWorkflowUrl(aWfStatus.getWorkflowId(), wfStubs.getOptions().getTarget(), client.getOptions().getNamespace()));
-
-            workflowStatii.add(aWfStatus);
-        }
-
-        for (WorkflowStatus wfStatus : workflowStatii) {
-            logger.debug("Workflows " + wfStatus.getWorkflowId() + " " + wfStatus.getWorkflowStatus() + " " + wfStatus.getUrl());
-
-            model.addAttribute("workflows",workflowStatii);
-        }
-        return new String("money-transfer-details");
-    } // End getMoneyTransfer Details
 
     @GetMapping("/hello")
     public String getHello() {
